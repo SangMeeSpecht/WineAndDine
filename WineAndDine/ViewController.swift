be6058fb679a9775ff687600b92c4c801fd0925b
SangMee Specht <spechtse@miamioh.edu>
2017-05-22 15:01:01 -0500

    Initial Commit

---
+++ WineAndDine/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WineAndDine
+//
+//  Created by SangMee Specht on 5/22/17.
+//  Copyright © 2017 SangMee Specht. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
